fd3bfc6152fa838556c288bb28090407a7a246e6
Grigorii <genovikov93@gmail.com>
2024-08-07 15:37:50 +0200

    Correct aleph request params encoding

--- third_party/aleph.py
+++ third_party/aleph.py
@@ -1,6 +1,4 @@
 from typing import Tuple
-import urllib.parse
-from types import MappingProxyType
 
 from third_party.base import ThirdPartyRequest
 
@@ -9,7 +7,7 @@ class AlephRequest(ThirdPartyRequest):
     async def search(
         self, fts: str, search_type: str = "", *args, **kwargs
     ) -> Tuple[int, dict]:
-        params = {"q": urllib.parse.quote(fts), "limit": 50, "offset": 0}
+        params = {"q": fts, "limit": 50, "offset": 0}
         if search_type == "ogrn":
             params["filter:schemata"] = "Company"
         else:
